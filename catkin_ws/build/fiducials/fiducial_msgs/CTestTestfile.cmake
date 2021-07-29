a799fa1744fe6f6b381f39747bce44e2e01df345
Natalia Borges <natioliveira97@hotmail.com>
2021-07-28 22:34:31 -0300

    try

--- catkin_ws/build/fiducials/fiducial_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natalia/multi-camera/catkin_ws/src/fiducials/fiducial_msgs
-# Build directory: /home/natalia/multi-camera/catkin_ws/build/fiducials/fiducial_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/skeleton_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/natalia/multi-camera/catkin_ws/src/skeleton_markers
-# Build directory: /home/natalia/multi-camera/catkin_ws/build/skeleton_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
