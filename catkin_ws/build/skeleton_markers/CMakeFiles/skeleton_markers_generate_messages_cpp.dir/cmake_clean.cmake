44623f690772016c87341dbea286c12b6de273a3
Natalia Borges <natioliveira97@hotmail.com>
2021-07-28 23:15:34 -0300

    try

--- catkin_ws/build/skeleton_markers/CMakeFiles/skeleton_markers_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/natalia/multi-camera/catkin_ws/devel/include/skeleton_markers/Skeleton.h"
-  "CMakeFiles/skeleton_markers_generate_messages_cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/skeleton_markers_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
